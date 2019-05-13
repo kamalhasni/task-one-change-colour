619fdbfee271f623a5a3d18745c2addcaddd4baa
Ameerul Arif <arif.amee@gmail.com>
2019-05-13 22:23:55 +0800

    Updates

--- precache-manifest.0c99bc9ea73a008b125e677f286f9980.js
+++ precache-manifest.0c99bc9ea73a008b125e677f286f9980.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d4b6a8aa60256121fafd7c6d67cf337",
+    "revision": "bab554cb7f532dee6118fcca65a10d86",
     "url": "/task-one-change-colour/index.html"
   },
   {
-    "revision": "3e3c6fb4e23da5375891",
+    "revision": "ea16128691f08b5e787c",
     "url": "/task-one-change-colour/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "edb724b626f56e8dc0ed",
+    "revision": "2840a3c4f0749f7f5e69",
     "url": "/task-one-change-colour/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "3e3c6fb4e23da5375891",
-    "url": "/task-one-change-colour/static/js/2.40cac05e.chunk.js"
+    "revision": "ea16128691f08b5e787c",
+    "url": "/task-one-change-colour/static/js/2.f4c3908e.chunk.js"
   },
   {
-    "revision": "edb724b626f56e8dc0ed",
-    "url": "/task-one-change-colour/static/js/main.ee127c21.chunk.js"
+    "revision": "2840a3c4f0749f7f5e69",
+    "url": "/task-one-change-colour/static/js/main.fbcb91cf.chunk.js"
   },
   {
     "revision": "2a23c60a928591d39ca3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/task-one-change-colour/precache-manifest.7287aa4febfd8414a0134c2e994ff6c8.js"
+  "/task-one-change-colour/precache-manifest.0c99bc9ea73a008b125e677f286f9980.js"
 );
 
 self.addEventListener('message', (event) => {
