6b93969b5e018d2f20f9ebcc709cdece87575806
Ameerul Arif <arif.amee@gmail.com>
2019-05-13 21:13:08 +0800

    Updates

---
+++ precache-manifest.3618e8b756d19cad97f064f2d317a161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b2a6e3c9a9876dbd413f89a92f51eb",
+    "url": "/change-colour/index.html"
+  },
+  {
+    "revision": "32adb08d0ee06df40d2d",
+    "url": "/change-colour/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f70ec44281f2ea3dad8e",
+    "url": "/change-colour/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "32adb08d0ee06df40d2d",
+    "url": "/change-colour/static/js/2.10f73c9b.chunk.js"
+  },
+  {
+    "revision": "f70ec44281f2ea3dad8e",
+    "url": "/change-colour/static/js/main.a62553b0.chunk.js"
+  },
+  {
+    "revision": "31b476183b88264924ab",
+    "url": "/change-colour/static/js/runtime~main.b73c6dc4.js"
+  }
+]);
