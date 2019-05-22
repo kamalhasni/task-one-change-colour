003f8c1ff85004b8ed37b655d831f8713899a12c
Abdullah Hasni <kamal.hasni@gmail.com>
2019-05-22 11:11:12 +0800

    update without redirect

--- src/components/tasks/task-two/Display.js
+++ src/components/tasks/task-two/Display.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import colors from '../../../data/colors.js'
-import { Jumbotron } from 'reactstrap'
+import { Jumbotron, Button } from 'reactstrap'
 
 class Display extends React.Component {
+  constructor(props) {
+    super(props);
+    this.redirectBack = this.redirectBack.bind(this);
+  }
+
   getText() {
     const { match: { params } } = this.props
 
@@ -16,15 +21,28 @@ class Display extends React.Component {
       } else {
         return colors.find(e => e.isDefault).value
       }
-    }    
+    }
+  }
+
+  redirectBack() {    
+    this.props.history.push('/task-two');
   }
 
   render() {
-    return (<div>
-        <Jumbotron>
-            <h1 className="display-3">{this.getText()}</h1>
-        </Jumbotron>
-      </div>)
+    return (<div className="container mt-5 pt-5">
+              <div className="row mb-3">
+                <div className="col-12">
+                  <Jumbotron>
+                      <h1 className="display-3">{this.getText()}</h1>
+                  </Jumbotron>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12 mb-4">
+                <Button className="m-3 btn-lg" color="info" onClick={this.redirectBack}>Back</Button>
+              </div>
+            </div>
+          </div>)
     }
 }
 
--- src/components/tasks/task-two/Main.js
+++ src/components/tasks/task-two/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 import colors from '../../../data/colors.js'
-import { Redirect } from 'react-router-dom'
+// import { Redirect } from 'react-router-dom'
 
 class Main extends React.Component {
   constructor(props) {
@@ -33,9 +33,8 @@ class Main extends React.Component {
   }
 
   redirect() {
-    this.setState({
-      redirect: true,
-    })
+    let path = '/task-two/coloring/' + this.state.selectedColor.key
+    this.props.history.push(path);
   }
 
   getColorFromButton(event) {
@@ -48,71 +47,61 @@ class Main extends React.Component {
   }
 
   render() {
-    if (this.state.redirect) {
-      return (
-        <div>
-          <Redirect to={{
-                pathname: '/task-two/coloring/' + this.state.selectedColor.key,
-            }} />
-        </div>
-      )
-    } else {
-      return (
-        <div className="container mt-5 pt-5">
-          <div className="row mb-3">
-            <div className="col-12">
-              <h3>Task Two: Router</h3>
-              <hr/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12 mb-4">
-              <h6 className="card-title">OBJECTIVES:</h6>
-              <hr/>
-              <ol>
-                <li>Alot</li>
-              </ol>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12">
-                <h6 className="card-title">COLORING BUTTONS:</h6>
-                <hr/>
-                {
-                  colors.map(function (item, index) {
-                    return <Button key={index} index={index} className="m-3 btn-lg" color={item.key} onClick={(e) => this.colorBeforeToggelModal(e)}>{item.value}</Button>
-                  }, this)
-                }
-                <Modal isOpen={this.state.isOpen} toggle={this.toggle} className={this.props.className}>
-                  <ModalHeader toggle={this.toggle}>React Learning Modal</ModalHeader>
-                  <ModalBody className={this.state.colorClass}>
-                    {this.state.colorName}
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button color="secondary" onClick={this.toggle}>Close</Button>
-                  </ModalFooter>
-                </Modal>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12">
-                <h6 className="card-title">REDIRECT BUTTONS:</h6>
-                <hr/>
-                {/* {
-                  colors.reverse().slice(0, 2).map(function (item, index) {
-                    return <Button key={index} className="m-3 btn-lg" color={item.key} onClick={this.redirect}>{item.value}</Button>
-                  }, this)
-                } */}
-                {
-                  colors.slice(0, 2).map(function (item, index) {
-                    return <Button key={index} className="m-3 btn-lg" color={item.key} onClick={this.redirect}>{item.value}</Button>
-                  }, this)
-                }
-            </div>
+    return (
+      <div className="container mt-5 pt-5">
+        <div className="row mb-3">
+          <div className="col-12">
+            <h3>Task Two: Router</h3>
+            <hr/>
           </div>
         </div>
-      )
-    }
+        <div className="row">
+          <div className="col-md-12 mb-4">
+            <h6 className="card-title">OBJECTIVES:</h6>
+            <hr/>
+            <ol>
+              <li>Alot</li>
+            </ol>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+              <h6 className="card-title">COLORING BUTTONS:</h6>
+              <hr/>
+              {
+                colors.map(function (item, index) {
+                  return <Button key={index} index={index} className="m-3 btn-lg" color={item.key} onClick={(e) => this.colorBeforeToggelModal(e)}>{item.value}</Button>
+                }, this)
+              }
+              <Modal isOpen={this.state.isOpen} toggle={this.toggle} className={this.props.className}>
+                <ModalHeader toggle={this.toggle}>React Learning Modal</ModalHeader>
+                <ModalBody className={this.state.colorClass}>
+                  {this.state.colorName}
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="secondary" onClick={this.toggle}>Close</Button>
+                </ModalFooter>
+              </Modal>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+              <h6 className="card-title">REDIRECT BUTTONS:</h6>
+              <hr/>
+              {/* {
+                colors.reverse().slice(0, 2).map(function (item, index) {
+                  return <Button key={index} className="m-3 btn-lg" color={item.key} onClick={this.redirect}>{item.value}</Button>
+                }, this)
+              } */}
+              {
+                colors.slice(0, 2).map(function (item, index) {
+                  return <Button key={index} className="m-3 btn-lg" color={item.key} onClick={this.redirect}>{item.value}</Button>
+                }, this)
+              }
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
